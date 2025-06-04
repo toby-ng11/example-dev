d76b922f4d668959904ea21cbcdb984aec583d22
tnguyen-tech <149532296+tnguyen-tech@users.noreply.github.com>
2025-06-04 15:36:12 -0400

    v0.2.1: fix vite server dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,33 +5,39 @@ import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
-    ],
-    esbuild: {
-        jsx: 'automatic',
-    },
-    resolve: {
-        alias: {
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+export default defineConfig(({ command }) => {
+    const isDev = command === 'serve';
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.tsx'],
+                ssr: 'resources/js/ssr.tsx',
+                refresh: true,
+            }),
+            react(),
+            tailwindcss(),
+        ],
+        esbuild: {
+            jsx: 'automatic',
         },
-    },
-    server: {
-        https: {
-            key: fs.readFileSync(resolve(__dirname, 'certs/p2q.key')),
-            cert: fs.readFileSync(resolve(__dirname, 'certs/p2q.crt')),
+        resolve: {
+            alias: {
+                'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+            },
         },
-        host: '0.0.0.0',
-        hmr: {
-            protocol: 'wss',
-            host: 'p2q.centura.local',
-        },
-    },
+        server: isDev
+            ? {
+                  https: {
+                      key: fs.readFileSync(resolve(__dirname, 'certs/p2q.key')),
+                      cert: fs.readFileSync(resolve(__dirname, 'certs/p2q.crt')),
+                  },
+                  host: '0.0.0.0',
+                  hmr: {
+                      protocol: 'wss',
+                      host: 'p2q.centura.local',
+                  },
+              }
+            : undefined,
+    };
 });
