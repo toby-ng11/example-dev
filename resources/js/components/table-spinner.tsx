8dba91677190164344b3f245ccc3d1069b55f07f
toby-ng11 <nktue2000@gmail.com>
2025-07-17 22:59:59 -0400

    v0.6.1: fix bugs

--- resources/js/components/table-spinner.tsx
+++ resources/js/components/table-spinner.tsx
@@ -1,19 +1,9 @@
 import { Loader2 } from 'lucide-react';
-import { motion } from 'motion/react';
 
 export function DataTableLoadingSpinner() {
     return (
         <div className="flex h-40 items-center justify-center">
-            <motion.div
-                animate={{ rotate: 360 }}
-                transition={{
-                    repeat: Infinity,
-                    ease: 'linear',
-                    duration: 1,
-                }}
-            >
-                <Loader2 className="text-muted-foreground h-6 w-6 animate-spin" />
-            </motion.div>
+            <Loader2 className="text-muted-foreground h-6 w-6 animate-spin" />
         </div>
     );
 }
